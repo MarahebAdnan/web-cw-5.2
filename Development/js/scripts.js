a8cb666fd1dd9710b82fbc5fa7f63a3344827581
مراحب عدنان يوسف - 577 <marahebx001@gmail.com>
2020-06-29 23:40:07 +0300

    kinda done (JUST THE PLUS DONOT WORK) so im sorry :C

---
+++ Development/js/scripts.js
@@ -0,0 +1,127 @@
+let pizza="1-pizza";
+let fries="2-fries";
+
+console.log(pizza);
+console.log(fries);
+
+let choose=prompt("choose any dish you want from the console ");
+switch(choose) {
+    case '1':
+      console.log(pizza);
+      break;
+    case '2':
+        console.log(fries);
+      break;
+    default:
+     console.log("your order is not include in the menu");
+  }
+const pro = "enter any number"
+
+ let four = 
+ prompt(pro);
+
+ let two =
+ prompt ("choose one +   /   *   - ");
+
+ let one = 
+ prompt (pro)
+ if (four && one) {
+
+ if(two=='+')
+ { let answer=four+one
+     console.log(answer);
+ }
+
+ 
+ else if(two=='-')
+ {
+    let answer=four-one
+    console.log(answer);
+ }
+
+ 
+ else if(two=='*')
+ {
+    let answer=four*one
+    console.log(answer);
+ }
+ else if(two=='/')
+ {
+    let answer=four/one
+    console.log(answer);
+ }
+} else {
+    console.log(NaN)
+}
+
+ 
+
+ let age=
+ prompt("enter your age");
+ switch(age) {
+     case '3': alert('روضة');
+       break;
+
+       case '4': alert('روضة');
+       break;
+
+       case '5': alert('روضة');
+       break;
+
+    case '6': alert('ابتدائي');
+       break;  
+
+  case' 7' : alert('ابتدائي');
+   break;
+
+   case '8' : alert('ابتدائي');
+     break;
+
+case' 9' :alert('ابتدائي');
+ break;
+ case '10' :alert('ابتدائي');
+    break;
+    case '11' :alert('متوسط');
+    break;
+    case '12' :alert('متوسط');
+    break;
+    case '13' :alert('متوسط');
+    break;
+    case '14' :alert('متوسط');
+    break;
+    case '15' :alert('ثانوي');
+    break;
+    case '16' :alert('ثانوي');
+    break;
+    case '17' :alert('ثانوي');
+
+    case '18' :alert('جامعة');
+    break;
+    case '19' :alert('جامعة');
+    break;
+    case '20' :alert('جامعة');
+    break;
+    case '21' :alert('جامعة');
+     break;
+     case '22' :alert('جامعة');
+     break;
+     case '23' :alert('جامعة');
+     break;
+     case '24' :alert('جامعة');
+     break;
+     case '25' :alert('جامعة');
+      break;
+     
+      case '26' :alert('جامعة');
+      break;
+      case '27' :alert('جامعة');
+       break;
+     default:
+         alert("انت لست بأي مرحلة دراسية :D");
+   }
+
+
+
+
+
+
